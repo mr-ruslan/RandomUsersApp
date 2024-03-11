2199c671a3159989e277537ff30296b5cf157013
Ruslan Morozov <r.morozov@g.nsu.ru>
2024-03-12 03:19:39 +0700

    fix path

--- app/src/main/java/ru/nsu/morozov/randomusers/data/remote/RandomUserApi.kt
+++ app/src/main/java/ru/nsu/morozov/randomusers/data/remote/RandomUserApi.kt
@@ -1,10 +1,12 @@
 package ru.nsu.morozov.randomusers.data.remote
 
 import retrofit2.http.GET
+import retrofit2.http.Headers
 import retrofit2.http.Query
 import ru.nsu.morozov.randomusers.data.remote.dtos.GetRandomUsersResponse
 
 interface RandomUserApi {
-    @GET("/")
-    suspend fun getRandomUsers(@Query("usersCount") usersCount: Long): GetRandomUsersResponse
+    @GET("api/")
+    @Headers("Accept: application/json")
+    suspend fun getRandomUsers(@Query("results") usersCount: Long): GetRandomUsersResponse
 }
--- app/src/main/java/ru/nsu/morozov/randomusers/data/remote/repository/RandomUsersRepositoryImpl.kt
+++ app/src/main/java/ru/nsu/morozov/randomusers/data/remote/repository/RandomUsersRepositoryImpl.kt
@@ -11,7 +11,7 @@ import java.util.concurrent.TimeUnit
 class RandomUsersRepositoryImpl : RandomUsersRepository {
     private companion object {
 
-        const val BASE_URL = "https://randomuser.me/api/"
+        const val BASE_URL = "https://randomuser.me/"
         const val CONNECT_TIMEOUT = 10L
         const val WRITE_TIMEOUT = 10L
         const val READ_TIMEOUT = 10L
